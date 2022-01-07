edc395b96c581c2011a7decb539724233db705ce
Kolawole Ademola <64567320+demola234@users.noreply.github.com>
2022-01-07 05:11:28 +0100

    Initial Commit

---
+++ NetflixUi/NetflixUiApp.swift
@@ -0,0 +1,17 @@
+//
+//  NetflixUiApp.swift
+//  NetflixUi
+//
+//  Created by Oluwasegun Kolawole Ademola on 07/01/2022.
+//
+
+import SwiftUI
+
+@main
+struct NetflixUiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
